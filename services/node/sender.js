061337c8a28e516df22a13626ece3d2fc59ba4c3
randomsolutions <randomsolutions@hotmail.com>
2018-02-06 20:29:52 -0800

    Lora Message

--- services/node/sender.js
+++ services/node/sender.js
@@ -1,11 +1,12 @@
+var sqlite3=require('sqlite3-promise')
+var db = new sqlite3.Database('/share/balloon.db');
 var SX127x = require('sx127x');
 
 var sx127x = new SX127x({
   frequency: 433E6,
   resetPin:	6,
   dio0Pin:	5,
-  spreadingFactor: 12,
-  signalBandwidth: 15.6E3//10.4E3
+  spreadingFactor: 12
 });
 
 var count = 0;
@@ -21,20 +22,26 @@ sx127x.open(function(err) {
   // send a message every second
   setInterval(function() {
     console.log('write: ' + count);
-    const buf = Buffer.allocUnsafe(22);
-    buf.writeUInt16BE(count, 0);//Count 16
+    const buf = Buffer.allocUnsafe(25);
+
+    var latestGpsReading = "SELECT Latitude, Longitude, ReadingTime, FixStatus, MslAltitude, SpeedOverGround FROM GpsReading ORDER BY 1 DESC LIMIT 1";
+    var maxAltitude = "SELECT MAX(CAST(MslAltitude AS REAL)) FROM GpsReading WHERE MslAltitude <> ''";
+    var latestAltitudeReading = "select ReadingTime, TemperatureInFahrenheit, PressureInPascals, AltitudeInFeet FROM AltitudeReading ORDER BY 1 DESC LIMIT 1";
+    var maxAltitude = "SELECT MAX(CAST(MslAltitude AS REAL)) FROM GpsReading WHERE MslAltitude <> ''";
+    buf.writeUInt16BE(count, 0);//Count 16 Unsigned (0->65,535)
+    buf.writeUInt8(count, 0);//BITS -> Connected|GpsFixed|AltitudeWorking|CameraWorking|CellWorking|ExternalTempWorking|GpsWorking|GyroWorking
     buf.writeFloatBE(count, 0);//Lat 32 (32.980277)
     buf.writeFloatBE(count, 0);//Lng 32 (-117.058365)
-    buf.writeUInt16BE(count, 0);//Altitude Meters 16
-    buf.writeUInt16BE(count, 0);//Max Altitude Meters 16
-    buf.writeInt8(count, 0);//Temp 8
-    buf.writeInt8(count, 0);//Min Temp 8
-    buf.writeInt8(count, 0);//Connected 8 - Bit
-    buf.writeInt8(count, 0);//Strengh DB 8
-    buf.writeInt8(count, 0);//Battery % Full 8
-    buf.writeInt8(count, 0);//Volts 8
-    buf.writeInt8(count, 0);//Fix Status 8 - Bit
-    buf.writeInt8(count, 0);//Speed Over Ground 8
+    buf.writeUInt16BE(count, 0);//MSL Altitude Meters 16 Unsigned (0->65,535)
+    buf.writeUInt16BE(count, 0);//MSL Max Altitude Meters 16 Unsigned (0->65,535)
+    buf.writeUInt16BE(count, 0);//Altimeter Altitude Meters 16 Unsigned (0->65,535)
+    buf.writeUInt16BE(count, 0);//Altimeter Max Altitude Meters 16 Unsigned (0->65,535)
+    buf.writeInt8(count, 0);//Internal Temp In Fahrenheit 8 (-128=>127)
+    buf.writeInt8(count, 0);//External Temp In Fahrenheit 8 (-128=>127)
+    buf.writeInt8(count, 0);//External Min In Fahrenheit Temp 8 (-128=>127)
+    buf.writeInt8(count, 0);//Strengh DB 8 (-128=>127)
+    buf.writeUInt8(count, 0);//Battery % Full 8 (0=>255)
+    buf.writeUInt8(count, 0);//Speed Over Ground 8 Kilometers Per Hour (0->255)
     console.log(buf);
     count++;
     sx127x.write(buf, function(err) {
